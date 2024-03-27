4d7016ed8e6f91c4f112f98d59483a468796ce5d
Hap1930 <nasirlis024@gmail.com>
2024-03-27 21:09:27 +0300

    localhost

--- mypr/mypr/settings.py
+++ mypr/mypr/settings.py
@@ -124,6 +124,8 @@ LOGIN_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+ALLOWED_HOSTS = ['*']
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
